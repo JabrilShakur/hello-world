0b5d259056ba729be04b18913739c485794085ad
JabrilShakur <55251121+JabrilShakur@users.noreply.github.com>
2019-10-31 16:23:17 -0400

    Add files via upload

---
+++ 31.py
@@ -0,0 +1,14 @@
+numberlist=
+sumInt=0
+ovgInt=0.0
+
+for x in range(0,20)
+    num= int(input("Enter a number"))
+    numberlist.append(num)
+    sumInt=sumInt + numberList[x]
+print(sumInt)
+
+avgInt=(sumInt/20)
+
+print("the sum of the integers you have entered is", sumInt
+print("The average of the intergers you have entered is") 
